d5617523fa30f031ef5f282997176ea5fad1c3c0
linpeng6 <1204059398@qq.com>
2021-12-30 10:41:47 +0800

    feat create draggable file

---
+++ src/pages/drag/html-draggable/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '@comp/layout/container';
+const index: React.FC<{}> = () => {
+  return (
+    <Container>
+      <div className="drag-html-draggable"></div>
+    </Container>
+  );
+};
+
+export default index;
